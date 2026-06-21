c497c95e28b395aca843ce4a4b389735b252d2ce
BAYUNZIYUE <BAYUNZIYUE@users.noreply.github.com>
2026-06-21 12:23:19 +0000

    调整: 弹窗宽度适配手机——移动端 11/12 宽度

--- ui/App/components/Modal.jsx
+++ ui/App/components/Modal.jsx
@@ -11,7 +11,7 @@ const Modal = ({title, content, isOpen, actions = null}) => {
             <div className="bg-black bg-opacity-75 fixed top-0 left-0 z-10 w-full min-h-screen">
                 <Panel
                     title={title}
-                    className="w-1/3 mx-auto mt-6"
+                    className="w-11/12 md:w-1/2 lg:w-1/3 mx-auto mt-6"
                     content={content}
                     actions={actions}
                 />
